6ef39cfd5fe930b1babecb153b7248d9de4824b5
Francis Begyn <francis.begyn@gmail.com>
2019-01-10 20:30:01 +0100

    More zsh plugins

--- .config/zsh/zsh_plugins.sh
+++ .config/zsh/zsh_plugins.sh
@@ -1,6 +1,16 @@
 source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
 source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
 fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-agkozak-zsh-prompt/agkozak-zsh-prompt.plugin.zsh
+fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-agkozak-zsh-prompt )
+source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andrewferrier-SLASH-fzf-z/fzf-z.plugin.zsh
+fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andrewferrier-SLASH-fzf-z )
+source /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-autoenv/autoenv.plugin.zsh
+fpath+=( /home/francis/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-autoenv )
