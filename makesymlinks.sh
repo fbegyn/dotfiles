7c80b45764f65eaa8efa12491462bead5bce8173
Francis Begyn <francis.begyn@gmail.com>
2019-01-30 03:51:57 +0100

    Better script that is dynamic

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home DIRectory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
+DIR=${PWD}
+OLDDIR="${DIR}_old"            # old dotfiles backup DIRectory
 
-dir=$HOME/dotfiles                    # dotfiles directory
-olddir=$HOME/dotfiles_old             # old dotfiles backup directory
+# create dotfiles_old in homeDIR
+if [ ! -d $OLDDIR ]; then
+  echo "Creating $OLDDIR for backup of any existing dotfiles in ~"
+  mkdir -p $OLDDIR/.config
+  echo "...done"
+fi
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir/.config
+# change to the dotfiles DIRectory
+echo "Changing to the $DIR DIRectory"
+cd $DIR
 echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+printf "Starting linking process:\n\n"
 
-files=${PWD}/
 for f in ./.*; do
   if [ $f == "./." ] || [ $f == "./.." ] || [ $f == "./.git" ]; then
     continue
@@ -30,22 +32,21 @@ for f in ./.*; do
       FILE=${fc##*/}
       echo "Processing ~/.config/$FILE"
       if [ -f "$HOME/.config/$FILE" ] || [ -d "$HOME/.config/$FILE" ]; then
-        echo "Backing up $HOME/.config/$FILE"
-        mv "$HOME/.config/$FILE" "$olddir/.config/" || exit 1
+        echo "Backing up $HOME/.config/$FILE to $OLDDIR/.config"
+        mv "$HOME/.config/$FILE" "$OLDDIR/.config/" || exit 1
       fi
-      echo "Creating symlink to $dir/.config/$FILE in ~/.config/$FILE"
-      ln -s "$dir/.config/$FILE" "$HOME/.config/$FILE" || exit 1
+      echo "Creating symlink to $DIR/.config/$FILE in ~/.config/$FILE"
+      ln -s "$DIR/.config/$FILE" "$HOME/.config/$FILE" || exit 1
       printf ".config/$FILE done\n\n"
     done
   else
     echo "Processing ~/$FILE"
     if [ -f "$HOME/$FILE" ] || [ -d "$HOME/$FILE" ]; then
-      echo "Backing up $HOME/$FILE"
-      mv "$HOME/$FILE" "$olddir/" || exit 1
+      echo "Backing up $HOME/$FILE to $OLDDIR"
+      mv "$HOME/$FILE" "$OLDDIR/" || exit 1
     fi
-    echo "$dir/$FILE"
-    echo "Creating symlink to $dir/$FILE in ~/"
-    ln -s "$dir/$FILE" "$HOME/$FILE" || exit 1
+    echo "Creating symlink to $DIR/$FILE in ~/"
+    ln -s "$DIR/$FILE" "$HOME/$FILE" || exit 1
     printf "$FILE done\n\n"
   fi
 done
